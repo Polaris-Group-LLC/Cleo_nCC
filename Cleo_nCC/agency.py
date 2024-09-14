b9f77ee5da736bdf0e1fb095159e6ce7e656c461
jaybo1001 <138844854+jaybo1001@users.noreply.github.com>
2024-09-13 19:31:28 -0700

    chore: Update Cleo_nCC agency.py for dynamic server port and launch command

--- Cleo_nCC/agency.py
+++ Cleo_nCC/agency.py
@@ -18,5 +18,5 @@ agency = Agency([cleo, devid, browsing_agent,  # Add browsing_agent to the top-l
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.launch(server_name="0.0.0.0", server_port=port)
+    port = int(os.environ.get("PORT", 5151))
+    agency.demo_gradio(server_name="0.0.0.0", server_port=port)
